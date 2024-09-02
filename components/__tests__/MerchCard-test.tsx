64e17fca9c6e38ed828495148612e343f648ce3b
Gabriel Cabrejas <gabriel@cabrejas.net>
2024-09-02 17:20:18 +0200

    wip

--- components/__tests__/MerchCard-test.tsx
+++ components/__tests__/MerchCard-test.tsx
@@ -4,8 +4,8 @@ import MerchantCard from '../MerchCard'
 const order1 = {
   id: '1',
   merchantName: 'Amazon',
-  merchantImage: 'https://res.cloudinary.com/caskchain/image/upload/v1717158058/Sequra/amazon.png',
-  merchantLogo: 'https://cdn.iconscout.com/icon/free/png-512/free-amazon-1543560-1306063.png',
+  merchantImage: 'https://res.cloudinary.com/etc.png',
+  merchantLogo: 'https://cdn.iconscout.com/etc.png',
   date: '2021-10-01',
   nextDueAmount: 100,
   nextDueDate: '2021-11-01',
@@ -18,7 +18,9 @@ const order1 = {
 
 describe('<MerchantCard />', () => {
   test('Text renders correctly on MerchantCard', () => {
-    const screen = render(<MerchantCard data={order1} handlePress={Function.prototype} />)
+    const screen = render(
+      <MerchantCard data={order1} handlePress={Function.prototype} />
+    )
     screen.getByText('100 €')
     expect(screen.toJSON()).toMatchSnapshot()
   })
