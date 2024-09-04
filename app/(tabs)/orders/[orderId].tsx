fe0959ce9b13d440775852c5b25b8c66bdc02b35
Gabriel Cabrejas <gabriel@cabrejas.net>
2024-09-04 12:06:16 +0200

    add jsdoc comments

--- app/(tabs)/orders/[orderId].tsx
+++ app/(tabs)/orders/[orderId].tsx
@@ -2,12 +2,28 @@ import OrdersContainer from '@/containers/OrdersContainer'
 import { useLocalSearchParams, useRouter } from 'expo-router'
 import OrderDetails from '@/components/OrderDetails'
 
+/**
+ * Details component renders the order details screen.
+ *
+ * This component retrieves the `orderId` from the local search parameters and uses
+ * it to display the order details through the `OrderDetails` component. It also
+ * provides a back navigation handler to return to the orders list screen.
+ *
+ * @component
+ * @returns {JSX.Element} The JSX element representing the order details screen.
+ *
+ * @example
+ * // Basic usage of the Details component
+ * <Details />
+ */
 const Details = () => {
   const { orderId } = useLocalSearchParams()
   const router = useRouter()
+
   const handleBackPress = () => {
     router.replace('/orders')
   }
+
   return (
     <OrdersContainer>
       <OrderDetails orderId={orderId} handleBackPress={handleBackPress} />
